4458d60a60ee76bed1f52ceec936ffccf41692bb
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-09-19 01:34:04 +0100

    add shoppingCart

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ let btnHerbace = document.querySelector('.btnHerbace');
 let btnMousse = document.querySelector('.btnMousse');
 let inputsearch = document.querySelector('.inputsearch');
 let btnSearch = document.querySelector('.btnSearch');
-// affichage des données des produits depuis datas
+
+
+
+//la fonction affichage des données des produits depuis de datas
 const afficheProduit = (datas)=>{
     datas.forEach(element => {
-        containercarte.innerHTML +=`<div class="card col-11 col-md-5 col-lg-3 d-flex justify-content-center align-item-center p-0 m-4">
+        containercarte.innerHTML +=`<div class="card col-9 col-md-5 col-lg-3 d-flex justify-content-center align-item-center p-0 m-4">
         <div class="carteimg w-100 "> 
             <img src=${element.url} class="card-img-top" alt="...">
         </div>
@@ -41,27 +44,7 @@ let btnEtoile = document.querySelectorAll('.btnEtoile')
 //         element.classList.add('colorEtoile')
 //     })
 // });
-// for (let i = 0; i < btnEtoile.length; i++) {
-//     btnEtoile[i].addEventListener('click', ()=>{
-//         btnEtoile[i].classList.remove('fa-regular');
-//         btnEtoile[i].classList.remove('fa-star');
-//         btnEtoile[i].setAttribute('class','fa-solid fa-star')
-//         btnEtoile[i].classList.add('colorEtoile')
-//     })
-    
-    
-// }
 
-// for (let i = 0; i < btnEtoile.length; i++) {
-//     btnEtoile[i].addEventListener('click', () => {
-//         for (let j = 0; j <= i; j++) {
-//             btnEtoile[j].classList.remove('fa-regular');
-//             btnEtoile[j].classList.remove('fa-star');
-//             btnEtoile[j].setAttribute('class', 'fa-solid fa-star');
-//             btnEtoile[j].classList.add('colorEtoile');
-//         }
-//     });
-// }
 let isStarred = true;
 for (let i = 0; i < btnEtoile.length; i++) {
     btnEtoile[i].addEventListener('click', () => {
@@ -91,35 +74,55 @@ for (let i = 0; i < btnEtoile.length; i++) {
 }
 
 
-// les boutton filtrer
+// les boutton filtrer===========================================================
 
 btnAll.addEventListener('click', () => {
     containercarte.innerHTML ="";
     afficheProduit(datas);
 });
 btnArbre.addEventListener('click', () => {
-    const arbustes = datas.filter((element) => element.libelle === 'Arbuste');
+    const arbustes = datas.filter((element) => element.libelle == 'Arbuste');
     containercarte.innerHTML ="";
     afficheProduit(arbustes);
 });
 
 btnPalmier.addEventListener('click',()=>{
-    const Palmier = datas.filter((element) => element.libelle === 'Palmier');
+    const Palmier = datas.filter((element) => element.libelle == 'Palmier');
     containercarte.innerHTML ="";
     afficheProduit(Palmier);
 })
 btnConifere.addEventListener('click',()=>{
-    const Conifère = datas.filter((element) => element.libelle === 'Conifère');
+    const Conifère = datas.filter((element) => element.libelle == 'Conifère');
     containercarte.innerHTML ="";
     afficheProduit(Conifère);
 })
 btnHerbace.addEventListener('click',()=>{
-    const Herbacée = datas.filter((element) => element.libelle === 'Herbacée');
+    const Herbacée = datas.filter((element) => element.libelle == 'Herbacée');
     containercarte.innerHTML ="";
     afficheProduit(Herbacée);
 })
 btnMousse.addEventListener('click',()=>{
-    const Mousse = datas.filter((element) => element.libelle === 'Mousse');
+    const Mousse = datas.filter((element) => element.libelle == 'Mousse');
     containercarte.innerHTML ="";
     afficheProduit(Mousse);
 })
+
+// le boutton Search===========================================================
+
+btnSearch.addEventListener('click',()=>{
+    if (inputsearch.value == 'All') {
+        containercarte.innerHTML ="";
+        afficheProduit(datas);
+        inputsearch.value = ""
+    }
+    else{
+        const fil = datas.filter((element) => element.libelle == inputsearch.value);
+        containercarte.innerHTML ="";
+        afficheProduit(fil);
+        console.log(inputsearch.value);
+        console.log(fil);
+        inputsearch.value =""
+    }
+
+})
+
